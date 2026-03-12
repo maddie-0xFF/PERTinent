7af98954c23b6ce6b9593e548fff3f253f4a806e
maddie-0xFF <madelaine.ds.01@gmail.com>
2026-03-11 22:49:46 -0300

    added different fields to the task model

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from .base import Base
@@ -7,3 +7,28 @@ class Task(Base):
     __tablename__ = 'tasks'
 
     id = Column(Integer, primary_key=True, index=True)
+    project_id = Column(Integer, ForeignKey('projects.id'), nullable=False)
+    name = Column(String(150), nullable=False)
+    description = Column(text)
+    duration = Column(Integer, nullable=False)        # Duration in hours
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    project = relationship("Project", back_populates="tasks")
+
+    predecessors = relationship(
+        "dependency",
+        foreign_keys="[dependency.c.successor_id]",
+        back_populates="successor"
+    )
+    successors = relationship(
+        "dependency",
+        foreign_keys="[dependency.c.predecessor_id]",
+        back_populates="predecessor"
+    )
+
+    calculation = relationship("CalculationResult",
+                               uselist=False,
+                               back_populates="task",
+                               cascade="all, delete-orphan"
+    )
